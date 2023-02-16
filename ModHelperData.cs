60486c7a236d483404f2fb7fdc47d4788111325b
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-02-16 16:56:40 -0500

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+﻿namespace NoDartlingRNG
+{
+    public static class ModHelperData
+    {
+        public const string WorksOnVersion = "34.3";
+        public const string Version = "1.0.0";
+        public const string Name = "No Dartling RNG";
+        public const string Description =
+            "This mod makes dartling gunners have no RNG. <br><br>Thank you Apollo??? for the idea.";
+
+        public const string RepoOwner = "Jonyboylovespie";
+        public const string RepoName = "NoDartlingRNG";
+        public const string Icon = "FocusedFiringIcon.png";
+    }
+}
---
+++ NoDartlingRNG.cs
@@ -0,0 +1,36 @@
+﻿using Il2CppAssets.Scripts.Models;
+using BTD_Mod_Helper;
+using MelonLoader;
+using NoDartlingRNG;
+using Il2CppAssets.Scripts.Models.Towers.Mods;
+using BTD_Mod_Helper.Api.ModOptions;
+using BTD_Mod_Helper.Api.Data;
+using BTD_Mod_Helper.Extensions;
+using System.Linq;
+using Il2CppAssets.Scripts.Models.Towers.Weapons;
+using Il2CppAssets.Scripts.Models.Towers.Behaviors.Emissions;
+
+[assembly: MelonInfo(typeof(NoDartlingRNG.Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace NoDartlingRNG
+{
+    public class Main : BloonsTD6Mod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg(System.ConsoleColor.Green, "Dartling Gunners now have no RNG!");
+        }
+        public override void OnNewGameModel(GameModel gameModel, Il2CppSystem.Collections.Generic.List<ModModel> mods)
+        {
+            foreach (var towerModel in gameModel.towers)
+            {
+                if (towerModel.baseId == "DartlingGunner")
+                {
+                    towerModel.GetDescendants<RandomEmissionModel>().ForEach(angle => angle.angle = 1);
+                    towerModel.GetDescendants<EmissionWithOffsetsModel>().ForEach(angle => angle.randomRotationCone = 1);
+                }
+            }
+        }
+    }
+}
