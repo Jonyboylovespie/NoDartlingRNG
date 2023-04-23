8317ae9c4d9b0c49fc009241e04b6b469e49b5ed
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-04-23 13:05:08 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 {
     public static class ModHelperData
     {
-        public const string WorksOnVersion = "34.3";
+        public const string WorksOnVersion = "36.3";
         public const string Version = "1.0.0";
         public const string Name = "No Dartling RNG";
         public const string Description =
